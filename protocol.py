f9ce6ced3394841de16a84ddb66e58c16bea19e4
mpi3d <matthieu.charette@gmail.com>
2021-08-24 20:03:47 +0200

    Fix for 5117

--- protocol.py
+++ protocol.py
@@ -120,7 +120,7 @@ class USBProtocol(Protocol):
         for i in range(0, length, 0x40):
             self.device.write(self.endpoint_out, data[i:i + 0x40], timeout)
 
-    def read(self, size: int = 0x4000, timeout: Optional[float] = 5) -> bytes:
+    def read(self, size: int = 0x10000, timeout: Optional[float] = 5) -> bytes:
         timeout = 0 if timeout is None else round(timeout * 1000)
 
         return self.device.read(self.endpoint_in, size, timeout).tobytes()
