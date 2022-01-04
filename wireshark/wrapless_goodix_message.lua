f5a03bc98bc05969f10ef662934f8692d50bb53d
Elia Geretto <elia.f.geretto@gmail.com>
2022-01-04 16:29:00 +0100

    Update wrapless protocol dissector using 5395

--- wireshark/wrapless_goodix_message.lua
+++ wireshark/wrapless_goodix_message.lua
@@ -2,6 +2,7 @@ protocol = Proto("goodix", "Goodix Fingerprint Sensor Message Protocol")
 
 cmd0_field = ProtoField.uint8("goodix.cmd0", "Command 0", base.HEX, nil, 0xf0)
 cmd1_field = ProtoField.uint8("goodix.cmd1", "Command 1", base.HEX, nil, 0x0e)
+contd_field = ProtoField.bool("goodix.contd", "Continued", 8, nil, 0x1)
 length_field = ProtoField.uint16("goodix.length", "Length", base.DEC)
 checksum_field = ProtoField.uint8("goodix.checksum", "Checksum", base.HEX)
 
@@ -30,8 +31,18 @@ reset_number = ProtoField.uint16("goodix.reset.number", "Sensor Reset Number")
 register_multiple = ProtoField.bool("goodix.register.multiple", "Multiple Addresses")
 register_address = ProtoField.uint16("goodix.register.address", "Base Address", base.HEX)
 
-psk_flags = ProtoField.uint32("goodix.psk.flags", "PSK Flags", base.HEX)
-psk_length = ProtoField.uint32("goodix.psk.length", "PSK Lenght")
+psk_msg_type = ProtoField.uint32("goodix.psk_msg.type", "PSK message type", base.RANGE_STRING,
+    {{0xb001, 0xb001, "SGX Sealed PSK"}, {0xb002, 0xb002, "Encrypted-Signed PSK"}, {0xb003, 0xb003, "PSK SHA256 Hash"}})
+psk_msg_length = ProtoField.uint32("goodix.psk_msg.length", "PSK message length", base.UNIT_STRING, {" bytes"})
+psk_msg_content = ProtoField.bytes("goodix.psk_msg.content", "PSK message content", base.SPACE)
+
+gtls_type = ProtoField.uint32("goodix.gtls.type", "TLS Handshake message type", base.RANGE_STRING,
+    {{0xff01, 0xff01, "Client hello (client_random)"},
+     {0xff02, 0xff02, "Server identity (server_random | server_identity)"},
+     {0xff03, 0xff03, "Client done (client_identity | 0xeeeeeeee)"},
+     {0xff04, 0xff04, "Server done"}})
+gtls_length = ProtoField.uint32("goodix.gtls.length", "TLS Handshake message length", base.UNIT_STRING, {" bytes"})
+gtls_content = ProtoField.bytes("goodix.gtls.content", "TLS Handshake content", base.SPACE)
 
 firmware_offset = ProtoField.uint32("goodix.firmware.offset", "Firmware Offset")
 firmware_length = ProtoField.uint32("goodix.firmware.length", "Firmware Lenght")
@@ -47,11 +58,12 @@ mode = ProtoField.uint8("goodix.mode", "Mode", base.RANGE_STRING,
      {0x10, 0xf0, "FF"}})
 base_type = ProtoField.uint8("goodix.base_type", "Base Type")
 
-protocol.fields = {pack_flags, cmd0_field, cmd1_field, length_field, checksum_field, ack_cmd, ack_true, ack_config,
+protocol.fields = {pack_flags, cmd0_field, cmd1_field, contd_field, length_field, checksum_field, ack_cmd, ack_true, ack_config,
                    success, failed, number, version, enable_chip, sleep_time, mcu_state_image, mcu_state_tls,
                    mcu_state_spi, mcu_state_locked, reset_sensor, reset_mcu, reset_sensor_copy, reset_number,
                    register_multiple, register_address, read_length, powerdown_scan_frequency, config_sensor_chip, mode,
-                   base_type, psk_flags, psk_length, firmware_offset, firmware_length, firmware_checksum}
+                   base_type, psk_msg_type, psk_msg_length, psk_msg_content, gtls_type, gtls_length, gtls_content,
+                   firmware_offset, firmware_length, firmware_checksum}
 
 function extract_cmd0_cmd1(cmd)
     return bit.rshift(cmd, 4), bit.rshift(cmd % 16, 1)
@@ -68,15 +80,16 @@ function get_cmd_name(cmd)
 end
 
 commands = {
-    [0x0] = {
-        category_name = "NOP",
+    [0x0] = { -- Correct
+        category_name = "PING",
+
         [0x0] = {
-            name = "nop",
+            name = "Ping",
             dissect_command = function(tree, buf)
             end
         }
     },
-    [0x2] = {
+    [0x2] = { -- Correct
         category_name = "IMA",
 
         [0] = {
@@ -89,7 +102,7 @@ commands = {
             end
         }
     },
-    [0x3] = {
+    [0x3] = { -- Correct
         category_name = "FDT",
 
         [1] = {
@@ -148,6 +161,7 @@ commands = {
     },
     [0x6] = {
         category_name = "SLE",
+
         [0] = {
             name = "MCU Switch To Sleep Mode",
             dissect_command = function(tree, buf)
@@ -171,8 +185,9 @@ commands = {
             end
         }
     },
-    [0x8] = {
+    [0x8] = { -- Correct
         category_name = "REG",
+
         [0] = {
             name = "Write Sensor Register",
             dissect_command = function(tree, buf)
@@ -185,7 +200,7 @@ commands = {
             dissect_command = function(tree, buf)
                 tree:add_le(register_multiple, buf(0, 1))
                 tree:add_le(register_address, buf(1, 2))
-                tree:add_le(read_length, buf(3, 1)):append_text(" bytes")
+                tree:add_le(read_length, buf(3, 2)):append_text(" bytes")
             end,
             dissect_reply = function(tree, buf)
             end
@@ -194,8 +209,8 @@ commands = {
     [0x9] = {
         category_name = "CHIP",
 
-        [0] = {
-            name = "Upload Config MCU Download Chip Config",
+        [0] = { -- Correct
+            name = "Set chip config",
             dissect_command = function(tree, buf)
                 tree:add_le(config_sensor_chip, buf(0, 1))
             end,
@@ -231,7 +246,7 @@ commands = {
     [0xa] = {
         category_name = "OTHER",
 
-        [1] = {
+        [1] = { -- Correct
             name = "Reset",
             dissect_command = function(tree, buf)
                 tree:add_le(reset_sensor, buf(0, 1))
@@ -240,12 +255,11 @@ commands = {
                 tree:add_le(sleep_time, buf(1, 1))
             end,
             dissect_reply = function(tree, buf)
-                tree:add_le(success, buf(0, 1))
-                tree:add_le(reset_number, buf(1, 2))
+                -- irqstatus[0,1,2]
             end
         },
-        [2] = {
-            name = "MCU Erase App",
+        [2] = { -- Correct
+            name = "MCU Clear Flash",
             dissect_command = function(tree, buf)
                 tree:add_le(sleep_time, buf(1, 1))
             end,
@@ -253,14 +267,14 @@ commands = {
                 tree:add_le(success, buf(0, 1))
             end
         },
-        [3] = {
+        [3] = { -- Correct
             name = "Read OTP",
             dissect_command = function(tree, buf)
             end,
             dissect_reply = function(tree, buf)
             end
         },
-        [4] = {
+        [4] = { -- Correct
             name = "Firmware Version",
             dissect_command = function(tree, buf)
             end,
@@ -304,82 +318,76 @@ commands = {
     [0xc] = {
         category_name = "NOTI",
 
-        [2] = {
-            name = "Set DRV State",
-            dissect_command = function(tree, buf)
-            end,
-            dissect_reply = function(tree, buf)
-            end
-        },
-        [3] = {
-            name = "MCU Set Led State",
-            dissect_command = function(tree, buf)
-            end,
-            dissect_reply = function(tree, buf)
-            end
-        }
-
-    },
-    [0xd] = {
-        category_name = "TLSCONN",
-
         [0] = {
-            name = "Request TLS Connection",
+            name = "ESD Happened", -- Electro Static Discharge?
             dissect_command = function(tree, buf)
+            end,
+            dissect_reply = function(tree, buf)
+                -- irq[0,1]
             end
         },
         [1] = {
-            name = "MCU Get POV Image",
+            name = "Wake up",
             dissect_command = function(tree, buf)
-                -- Seemingly gives the same response over TLS as sending Ima.0 does,
-                -- but without reading a new image from the sensor. Not seen used,
-                -- untested.
             end,
             dissect_reply = function(tree, buf)
             end
         },
         [2] = {
-            name = "TLS Successfully Established",
-            dissect_command = function(tree, buf)
-            end
-        },
-
-        [3] = {
-            name = "POV Image Check",
+            name = "Press Power Button",
             dissect_command = function(tree, buf)
             end,
             dissect_reply = function(tree, buf)
             end
-        }
+        },
     },
-    [0xe] = {
-        category_name = "PROD",
-        [0] = {
-            name = "Preset Psk Write",
+    [0xd] = { -- Correct
+        category_name = "TLSHANDSHAKE",
+
+        [1] = {
+            name = "TLS handshake",
             dissect_command = function(tree, buf)
-                tree:add_le(psk_flags, buf(0, 4))
-                tree:add_le(psk_length, buf(4, 4))
+                tree:add_le(gtls_type, buf(0, 4))
+                tree:add_le(gtls_length, buf(4, 4))
+                tree:add(gtls_content, buf(8))
+            end,
+            dissect_reply = function(tree, buf)
+                tree:add_le(gtls_type, buf(0, 4))
+                tree:add_le(gtls_length, buf(4, 4))
+                tree:add(gtls_content, buf(8))
+            end
+        },
+    },
+    [0xe] = { -- Correct
+        category_name = "PROD",
+
+        [1] = {
+            name = "PSK setup write",
+            dissect_command = function(tree, buf)
+                tree:add_le(psk_msg_type, buf(0, 4))
+                tree:add_le(psk_msg_length, buf(4, 4))
+                tree:add(psk_msg_content, buf(8))
             end,
             dissect_reply = function(tree, buf)
                 tree:add_le(failed, buf(0, 1))
             end
         },
         [2] = {
-            name = "Preset Psk Read",
+            name = "PSK setup read",
             dissect_command = function(tree, buf)
-                tree:add_le(psk_flags, buf(0, 4))
-                tree:add_le(psk_length, buf(4, 4))
+                tree:add_le(psk_msg_type, buf(0, 4))
             end,
             dissect_reply = function(tree, buf)
                 tree:add_le(failed, buf(0, 1))
-                tree:add_le(psk_flags, buf(1, 4))
-                tree:add_le(psk_length, buf(5, 4))
+                tree:add_le(psk_msg_type, buf(1, 4))
+                tree:add_le(psk_msg_length, buf(5, 4))
+                tree:add(psk_msg_content, buf(9))
             end
         }
     },
     [0xf] = {
         category_name = "UPFW",
-        [0] = {
+        [0] = { -- Correct
             name = "Write Firmware",
             dissect_command = function(tree, buf)
                 tree:add_le(firmware_offset, buf(0, 4))
@@ -401,9 +409,7 @@ commands = {
         [2] = {
             name = "Check Firmware",
             dissect_command = function(tree, buf)
-                tree:add_le(firmware_offset, buf(0, 4))
-                tree:add_le(firmware_length, buf(4, 4))
-                tree:add_le(firmware_checksum, buf(8, 4))
+                tree:add_le(firmware_checksum, buf(4, 4))
             end,
             dissect_reply = function(tree, buf)
                 tree:add_le(success, buf(0, 1))
@@ -420,6 +426,7 @@ commands = {
         }
     }
 }
+
 function protocol.init()
     state_map = ByteArray.new()
     total_bytes = 0
@@ -443,6 +450,7 @@ function protocol.dissector(buffer, pinfo, tree)
 
     subtree:add_le(cmd0_field, buffer(0, 1))
     subtree:add_le(cmd1_field, buffer(0, 1))
+    subtree:add_le(contd_field, buffer(0, 1))
 
     if from_host then
         summary = "Command: " .. get_cmd_name(cmd_val)
@@ -529,121 +537,7 @@ function protocol.dissector(buffer, pinfo, tree)
     cmd_subtree.text = summary
 end
 
-DissectorTable.get("tls.port"):add(1, protocol)
-DissectorTable.get("tls.port"):add(1, protocol)
-
-DissectorTable.get("usb.protocol"):add_for_decode_as(protocol)
-DissectorTable.get("usb.product"):add_for_decode_as(protocol)
-DissectorTable.get("usb.device"):add_for_decode_as(protocol)
-
-goodix_pack = Proto("goodix.pack", "Goodix Fingerprint USB Package")
-goodix_pack_flags = ProtoField.uint8("goodix.pack.flags", "Flags", base.HEX)
-goodix_pack_length = ProtoField.uint16("goodix.pack.length", "Length", base.DEC)
-goodix_pack_ckecksum = ProtoField.uint8("goodix.pack.checksum", "Checksum", base.HEX)
-
-function goodix_pack.init()
-    state_map = 0
-    missing_bytes = 0
-    cache = {}
-end
-
-goodix_pack.fields = {goodix_pack_flags, goodix_pack_length, goodix_pack_ckecksum}
-
-function goodix_pack.dissector(buffer, pinfo, tree)
-    length = buffer:len()
-    if length == 0 then
-        return
-    end
-
-    flags_byte = buffer(0, 1)
-    local flags_int = flags_byte:le_uint()
-
-    pinfo.cols.protocol = "Goodix Pack"
-
-    local subtree = tree:add(goodix_pack, buffer(), "Goodix Message Pack")
-
-    local ccache = cache[pinfo.number]
-
-    if ccache == nil then
-        if missing_bytes > 0 then
-            pinfo.cols.info = string.format("Goodix Pack Reassembly, missing %d bytes", missing_bytes)
-            state_map = state_map .. buffer:bytes()
-            if buffer:len() < missing_bytes then
-                missing_bytes = missing_bytes - buffer:len()
-                cache[pinfo.number] = {
-                    complete = 0,
-                    missing = missing_bytes
-                }
-                return
-            else
-                new_buffer = ByteArray.tvb(state_map)(0):tvb("Reassembled TVB")
-                cache[pinfo.number] = {
-                    complete = 1,
-                    content = state_map
-                }
-                state_map = 0
-                missing_bytes = 0
-            end
-        else
-            new_buffer = buffer
-            cache[pinfo.number] = {
-                complete = 1
-            }
-        end
-    else
-        if ccache.complete and ccache.content then
-            new_buffer = ByteArray.tvb(ccache.content)(0):tvb("Reassembled TVB")
-        else
-            new_buffer = buffer
-        end
-    end
-
-    buffer = new_buffer
-
-    subtree:add_le(goodix_pack_flags, flags_byte)
-    length_bytes = buffer(1, 2)
-    subtree:add_le(goodix_pack_length, length_bytes):append_text(" bytes")
-    ckecksum_byte = buffer(3, 1)
-    subtree:add_le(goodix_pack_ckecksum, ckecksum_byte)
-
-    local length_int = length_bytes:le_uint()
-
-    pinfo.cols.info = string.format("Goodix Pack 0x%x %d", flags_int, buffer:len())
-
-    if flags_int == 0xa0 or flags_int == 0xb0 or flags_int == 0xb2 then
-        if length_int + 4 > buffer:len() then
-            state_map = buffer:bytes()
-            missing_bytes = length_int - (buffer:len() - 4)
-
-            pinfo.cols.info = string.format("Goodix Pack Fragment Start 0x%x %d", flags_int, buffer:len())
-            return
-        end
-    elseif ccache.complete == 0 then
-        pinfo.cols.info = string.format("Goodix Pack Fragment Continue %d, %d", buffer:len(), ccache.missing)
-        return
-    end
-
-    if flags_int == 0xa0 then
-        body_buffer = buffer(4, length_int):tvb()
-        second_dissector = Dissector.get("goodix")
-        second_dissector:call(body_buffer, pinfo, subtree)
-    elseif flags_int == 0xb0 then
-        Dissector.get("tls"):call(buffer(4, length_int):tvb(), pinfo, tree)
-    elseif flags_int == 0xb2 then
-        Dissector.get("tls"):call(buffer(4 + 9, length_int - 9):tvb(), pinfo, tree)
-    else
-        body_buffer = buffer(4, buffer:len() - 4):tvb()
-        cmd_subtree = subtree:add(goodix_pack, body_buffer())
-        pinfo.cols.info = string.format("Goodix Pack Unknown 0x%02x", flags_int)
-
-    end
-
-end
-
 usb_table = DissectorTable.get("usb.bulk")
-
 usb_table:add(0x000a, protocol)
-
 usb_table:add(0x00ff, protocol)
-
 usb_table:add(0xffff, protocol)
