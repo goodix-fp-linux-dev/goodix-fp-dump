7de815c2c76fddcce05bd1846c538018607c3338
Timarrr <tigrmango@gmail.com>
2022-04-16 17:34:33 +1000

    fixed another thing in script

--- addfp_5110.sh
+++ addfp_5110.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 python run_5110.py
 mogrify -crop 64x80+0+0 -format jpg ./fingerprint.pgm
-mv ./fingerprint.jpg /home/mango/fpr/$(cat ./id).jpg
+mkdir -p fpr
+mv ./fingerprint.jpg fpr/$(cat ./id).jpg
 echo $(($(cat ./id) + 1)) > ./id
 
