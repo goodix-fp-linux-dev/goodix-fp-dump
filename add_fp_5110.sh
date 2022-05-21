7b793d204d01c4060365fd79c1d1c9b613c4a2f3
Matthieu CHARETTE <matthieu.charette@gmail.com>
2022-05-21 08:36:58 +0200

    Format add_fp_5110.sh

---
+++ add_fp_5110.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python run_5110.py
+mogrify -crop 64x80+0+0 -format jpg fingerprint.pgm
+mkdir fpr
+mv fingerprint.jpg fpr/$(cat id).jpg
+echo $(($(cat id) + 1)) > id
--- addfp_5110.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python run_5110.py
-mogrify -crop 64x80+0+0 -format jpg ./fingerprint.pgm
-mkdir -p fpr
-mv ./fingerprint.jpg fpr/$(cat ./id).jpg
-echo $(($(cat ./id) + 1)) > ./id
-
