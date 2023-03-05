21059070b3e20d40447a7ea2ca6855c33f45e35f
Vasily Khoruzhick <anarsoul@gmail.com>
2023-03-05 14:54:04 -0800

    preprocessor: crop input images, use thresholding and mean filters
    - Dealing with black border adds a lot of corner cases, so just crop
    the image.
    - Use thresholding filter for detecting finger coverage. That also
    allows us to just drop bin 0 (black) and bin 255 (white) while doing
    histogram equalization and gets rid of white level clamping
    - Use mean filter to improve image sharpness
    - Refactor the code, move image processing into their own functions
    instead of abusing main
    - Write out intermediate images for debugging purposes
    
    Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

--- preprocessor.py
+++ preprocessor.py
@@ -2,18 +2,28 @@
 
 import tool
 import argparse
-import os
 
-# TODO: implement proper coverage detection
-# Current implementation relies on non-covered pixels to be much brighter
-# than actual fingerprint image
-def get_inverse_coverage(image):
-    notcovered = 0
+def crop(image, width, height, x, y, new_width, new_height):
+    res = []
+    for y1 in range(y, y + new_height):
+        idx = y1 * width + x
+        res = res + image[idx:idx + new_width]
+
+    return res
+
+def threshold_filter(image, low, high):
+    result = []
+    coverage = 0
     for pixel in image:
-        if pixel > 4000:
-            notcovered = notcovered + 1
+        if pixel < low:
+            result.append(0)
+        elif pixel > high:
+            result.append(4095)
+        else:
+            coverage = coverage + 1
+            result.append(pixel)
 
-    return notcovered
+    return result, coverage
 
 def get_histogram(image):
     res = [0] * 256
@@ -38,68 +48,49 @@ def get_level(cumul_hist, value):
 
     return len(cumul_hist)
 
-def remove_border(image, width, height):
-    for idx in range(width):
-        image[idx] = 0
-        image[(height - 1) * width + idx] = 0
-    for idx in range(height):
-        image[idx * width] = 0
-        image[idx * width + width - 1] = 0
+def mean_filter(image, width, height, mask_radius, mask = None):
+    res = []
+    for y in range(0, height):
+        for x in range(0, width):
+            cnt = 0
+            val = 0
+            for y2 in range(y - mask_radius, y + mask_radius + 1):
+                if y2 < 0 or y2 >= height:
+                    continue
+                for x2 in range(x - mask_radius, x + mask_radius + 1):
+                    if x2 < 0 or x2 >= width:
+                        continue
+                    if mask:
+                        weight = mask[(x2 - x - mask_radius) + (mask_radius * 2 + 1) * (y2 - y - mask_radius)]
+                    else:
+                        weight = 1
+                    cnt = cnt + weight
+                    val = val + image[y2 * width + x2] * weight
+            if cnt == 0:
+                cnt = 1
+            val = val / cnt
+            res.append(val)
 
-def main(args):
-    (bg_width, bg_height, bg_depth, bg_img) = tool.read_pgm(args.background)
-    (width, height, depth, raw_img) = tool.read_pgm(args.image)
+    return res
 
-    assert bg_width == width and bg_height == height
-    assert bg_depth == depth == 4095
-
-    remove_border(raw_img, width, height)
-    remove_border(bg_img, width, height)
-
+def subtract_image(bg_img, img, offset, bg_weight, img_weight):
     subtracted = []
-    total = width * height - width * 2 - height * 2 + 4
-    coverage = total - get_inverse_coverage(raw_img)
-    print(f"Coverage: {coverage} out of {total}, %d%%" % (100 * coverage / total))
-    for bg_pixel, raw_pixel in zip(bg_img, raw_img):
-        val = abs(1000 + raw_pixel - bg_pixel)
+    for bg_pixel, img_pixel in zip(bg_img, img):
+        val = offset + img_weight * img_pixel / 100 - bg_weight * bg_pixel / 100
         if val < 0:
             val = 0
         if val > 4095:
             val = 4095
         subtracted.append(val)
+    return subtracted
 
-    # Need to remove border again, because we added offset of 1000 while subtracting
-    # background from raw image
-    remove_border(subtracted, width, height)
-
-    hist = get_histogram(subtracted)
-    # There is really no values [0:15] in scanned image, so it's all border
-    hist[0] = hist[0] - width * 2 - height * 2 + 4
-    cumul_hist = get_cumul_histogram(hist)
-    # Black level starts at 1% of total pixels
-    black_lvl = get_level(cumul_hist, 1 * coverage / 100) * 16
-    print(f"Black level: {black_lvl}")
-    if black_lvl < 500:
-        black_lvl = 500
-        print(f"Clamping black level to {black_lvl}")
-
-    print("Subtracted image histogram: %s" % hist)
-
-    # White level starts at 99% of total pixels
-    white_lvl = get_level(cumul_hist, 99 * coverage / 100) * 16
-    print(f"Estimated white level: {white_lvl}")
-    if white_lvl > 1500:
-        white_lvl = 1500
-        print(f"Clamping white level to {white_lvl}")
-
-
+def hist_equalization(image, black_lvl, white_lvl):
     # map [black_lvl .. white_lvl] to [0 .. 4095]
     # pixel < black_lvl: pixel = 0
     # pixel > white_lvl: pixel = 4095
     # 4096 * (pixel - black_lvl) / (white_lvl - black_lvl)
-
     res = []
-    for pixel in subtracted:
+    for pixel in image:
         if pixel < black_lvl:
             pixel = 0
         elif pixel > white_lvl:
@@ -108,11 +99,57 @@ def main(args):
             pixel = int(4095 * (pixel - black_lvl) / (white_lvl - black_lvl))
         res.append(pixel)
 
-    hist = get_histogram(res)
-    hist[0] = hist[0] - width * 2 - height * 2 + 4
-    print("Result image histogram: %s" % hist)
+    return res
 
-    tool.write_pgm(res, height, width, "result.pgm")
+def main(args):
+    (bg_width, bg_height, bg_depth, bg_img) = tool.read_pgm(args.background)
+    (width, height, depth, raw_img) = tool.read_pgm(args.image)
+
+    assert bg_width == width and bg_height == height
+    assert bg_depth == depth == 4095
+
+    # Crop image to drop black border
+    raw_img = crop(raw_img, width, height, 1, 1, width - 2, height - 2)
+    bg_img = crop(bg_img, width, height, 1, 1, width - 2, height - 2)
+    width = width - 2
+    height = height - 2
+
+    hist = get_histogram(raw_img)
+    print("Raw image histogram: %s" % hist)
+
+    subtracted = subtract_image(bg_img, raw_img, 1000, 100, 100)
+    subtracted, coverage = threshold_filter(subtracted, 0, 1700)
+    total = width * height
+    print(f"Coverage: {coverage} out of {total}, %d%%" % (100 * coverage / total))
+
+    hist = get_histogram(subtracted)
+    print("Subtracted image histogram: %s" % hist)
+    total = width * height - hist[0] - hist[1] - hist[2] - hist[255] - hist[254] - hist[253]
+    # We don't want to account black and white pixels
+    hist[0] = 0
+    hist[255] = 0
+    cumul_hist = get_cumul_histogram(hist)
+
+    # Black level starts at 0.1% of total pixels
+    black_lvl = get_level(cumul_hist, 1 * total / 1000) * 16
+    print(f"Black level: {black_lvl}")
+
+    # White level starts at 99% of total pixels
+    white_lvl = get_level(cumul_hist, 99 * total / 100) * 16
+    print(f"Estimated white level: {white_lvl}")
+
+    res = hist_equalization(subtracted, black_lvl, white_lvl)
+
+    hist = get_histogram(res)
+    print("Histogram equalized image histogram: %s" % hist)
+
+    mean_1 = mean_filter(res, width, height, 1, [1, 2, 1, 2, 4, 2, 1, 2, 1])
+    mean_sub_1 = subtract_image(mean_1, res, 0, 100, 200)
+
+    tool.write_pgm(res, height, width, "stage-2-hist-eq.pgm")
+    tool.write_pgm(subtracted, height, width, "stage-1-subtracted.pgm")
+    tool.write_pgm(mean_1, height, width, "stage-3-mean.pgm")
+    tool.write_pgm(mean_sub_1, height, width, "result.pgm")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
